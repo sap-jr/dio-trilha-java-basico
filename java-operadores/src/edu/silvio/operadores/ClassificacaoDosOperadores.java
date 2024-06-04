00b901801a73681dbc0b69087777b9df97c5cccb
sapjr <silviojr2010@gmail.com>
2024-06-03 23:43:53 -0300

    Classificação dos operadores

---
+++ java-operadores/src/edu/silvio/operadores/ClassificacaoDosOperadores.java
@@ -0,0 +1,27 @@
+package edu.silvio.operadores;
+public class ClassificacaoDosOperadores {
+public static void main(String[] args){
+    //classe Operadores.java
+String nomeCompleto = "LINGUAGEM" + "JAVA";
+System.out.println(nomeCompleto);	
+//qual o resultado das expressoes abaixo?
+String concatenacao ="?"; 
+System.out.println(concatenacao);
+
+concatenacao = 1+1+1+"1";
+System.out.println(concatenacao);
+
+concatenacao = 1 + "1" + 1 + 1;
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+"1";
+System.out.println(concatenacao);
+
+concatenacao = "1"+1+1+1;
+System.out.println(concatenacao);
+
+concatenacao = "1"+(1+1+1);
+    
+    System.out.println(concatenacao);
+    }
+}
--- java-operadores/src/edu/silvio/operadores/Operadores.java
+++
@@ -1,6 +0,0 @@
-package edu.silvio.operadores;
-public class Operadores {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
