b92e0a82027e7e0d08cb7c50b887f194332f0043
sapjr <silviojr2010@gmail.com>
2024-06-08 22:59:20 -0300

    Todos os recursos organizados por pacotes e classes bem específicas, sem nem mesmo escrever uma linha de código.

---
+++ java-documentacao/src/Comentarios.java
@@ -0,0 +1,37 @@
+public class Comentarios {
+    public static void main(String[] args){
+        // Olá, eu sou um comentário em uma única linha
+        
+        /* Olá,
+        * Eu sou um comentario
+        * que posso ser mais detalhado 
+        * quando necessário
+        */
+    }
+    /** 
+         * Estas duas estrelinhas acima
+         * é para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!
+         */
+    public void metodo(){
+    }
+/*
+ * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou  multiplicar
+ * dois números
+ */
+public int somaMultiplica (int 1, int 2){
+    int r = 0;// r é igual ao resultado
+    String str = "m"; 
+    if (str == "m"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x;
+    }
+    return r;
+    }
+}
