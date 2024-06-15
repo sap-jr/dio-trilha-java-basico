7b10405b8d7aff6295e5b2c18b44e6836206f2e2
sapjr <silviojr2010@gmail.com>
2024-06-14 22:51:55 -0300

    Nem sempre executamos nosso programa Java pela IDE

---
+++ java-terminal-argumentos/src/edu/silvio/terminalargumentos/TerminalEArgumentos.java
@@ -0,0 +1,7 @@
+package edu.silvio.terminalargumentos;
+
+public class TerminalEArgumentos {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
